122b4606e84d710be4e522f27b5994713527ddd6
tangcheng.xu <tangcheng.xu@united-imaging.com>
2022-10-14 08:13:46 +0800

    base framwork

--- vue-admin/src/main.ts
+++ vue-admin/src/main.ts
@@ -2,5 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementPlus from 'element-plus';
+// import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/theme-chalk/index.css'
+import { Vue } from "vue-class-component";
 
-createApp(App).use(store).use(router).mount("#app");
+// createApp(App).use(store).use(router).mount("#app");
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(ElementPlus);
+app.mount("#app");
